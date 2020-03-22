5226cb6f20a66810bbc710f99e242311836a5611
Salted fish <qmdx00@gmail.com>
2020-03-22 16:44:47 +0800

    Update 02冒泡排序.js

--- 02冒泡排序.js
+++ 02冒泡排序.js
@@ -1,15 +1,22 @@
-var flag = true;//记录有没有换过位置，没有则为true
-for (var i=0;i<newArr.length-1;i++) {//一共需要比较的轮数
-	for(var j=0;j<newArr.length-i-1;j++) {//每次比较的次数
-		//count++;//比较次数
-		flag = false
-		if (newArr[j]>newArr[j+1]) {
-			var temp = newArr[j+1];
-			newArr[j+1] = newArr[j];
-			newArr[j] = temp;//change location
-		}
-	}
-	if (flag==true) {
-		break;
-	}
+/**
+ * bubble sort
+ * @param {Array} arr unsorted array
+ */
+function bubble_sort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let flag = true;
+        for (let j = 0; j < arr.length - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                flag = false;
+            }
+        }
+        if (flag) break;
+    }
 }
+
+let arr = [1, 3, 2, 4, 6, 5];
+bubble_sort(arr);
+console.log(arr);
